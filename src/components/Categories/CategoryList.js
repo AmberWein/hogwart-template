0c33ab7cdee8a90cc46cd5be0df6aaa5d4975a76
AmberWein <amberw.dev@gmail.com>
2025-08-13 09:44:22 +0300

    set a React Router by and add a route for /wiki/:category

--- src/components/Categories/CategoryList.js
+++ src/components/Categories/CategoryList.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import CategoryCard from './CategoryCard';
+import React from "react";
+import CategoryCard from "./CategoryCard";
+import { Link } from 'react-router-dom';
+
 
 const CategoryList = ({ categories }) => {
   return (
     <div className="category-list">
-        {/* add link to each of these */}
+      {/* add link to each of these */}
+      <Link to="/wiki/potions">
         <CategoryCard id="potions" categoryData={categories[0]} />
-        <CategoryCard id="charms" categoryData={categories[1]} />
+      </Link>
 
-      {categories.slice(2).map(category => (
-          <CategoryCard categoryData={category} />
-        ))}
+      <Link to="/wiki/charms">
+        <CategoryCard id="charms" categoryData={categories[1]} />
+      </Link>
+
+      {categories.slice(2).map((category) => (
+        <CategoryCard categoryData={category} />
+      ))}
     </div>
   );
-}
+};
 
 export default CategoryList;
